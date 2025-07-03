1b0aed1a1ea890de224e42e1ae23e2c7d9673b11
Yugal Agarwal <74638775+Yugal41735@users.noreply.github.com>
2025-07-04 00:19:26 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,59 @@
+# Resource Group Outputs
+output "resource_group_name" {
+  description = "Name of the resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "resource_group_location" {
+  description = "Location of the resource group"
+  value       = azurerm_resource_group.main.location
+}
+
+# Virtual Machine Outputs
+output "vm_name" {
+  description = "Name of the Linux virtual machine"
+  value       = azurerm_linux_virtual_machine.main.name
+}
+
+output "vm_public_ip" {
+  description = "Public IP address of the virtual machine"
+  value       = azurerm_public_ip.main.ip_address
+}
+
+output "vm_private_ip" {
+  description = "Private IP address of the virtual machine"
+  value       = azurerm_network_interface.main.private_ip_address
+}
+
+# SQL Server Outputs
+output "sql_server_name" {
+  description = "Name of the SQL Server"
+  value       = azurerm_mssql_server.main.name
+}
+
+output "sql_server_fqdn" {
+  description = "Fully qualified domain name of the SQL Server"
+  value       = azurerm_mssql_server.main.fully_qualified_domain_name
+}
+
+output "sql_database_name" {
+  description = "Name of the SQL Database"
+  value       = azurerm_mssql_database.main.name
+}
+
+# Storage Account Outputs
+output "storage_account_name" {
+  description = "Name of the storage account"
+  value       = azurerm_storage_account.main.name
+}
+
+output "storage_account_primary_blob_endpoint" {
+  description = "Primary blob endpoint of the storage account"
+  value       = azurerm_storage_account.main.primary_blob_endpoint
+}
+
+output "storage_account_primary_access_key" {
+  description = "Primary access key of the storage account"
+  value       = azurerm_storage_account.main.primary_access_key
+  sensitive   = true
+}
